3d7feff16085b8308e8d05f4f677fbf0dda21d1c
RUVIVAF <valervfi@gmail.com>
2020-10-23 17:40:45 -0500

    test

---
+++ Programa01_RVVF/Programa01_RVVF.ino
@@ -0,0 +1,94 @@
+
+/*
+ * Ruiz Villalba Valentina Fabienne
+ * Prueba programa 01 con puente H
+ * Octubre 2020
+ * ESP32 TRA 
+ */
+
+
+/*
+ *--------------------------------------- 
+ *          Pines y variables 
+ * --------------------------------------
+ */
+ 
+ //Pines del motor
+int motor1Pin1 = 27; 
+int motor1Pin2 = 26; 
+int enable1Pin = 14; 
+
+// PWM propiedades
+int pwmFreq = 8000; //Periodo de 8ms
+int pwmChannel = 0;   
+int pwmRes = 8; //Resolución de 8 bits (2^8 = 254)
+
+//Variables de velocidad y dirección
+    int vel = 255;  // Con un ciclo de trabajo  del 100%
+    int dir= 0;
+/*
+ *------------------------------------ 
+ *        Funciones principales
+ *------------------------------------
+ */
+
+
+void setup() {
+    Serial.begin(115200);
+    
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(enable1Pin,OUTPUT);
+  
+    ledcSetup(pwmChannel,pwmFreq,pwmRes);
+
+    //Mandar señal PWM al pin enable del puente h
+    ledcAttachPin(enable1Pin,pwmChannel);
+
+    //Velocidad del motor
+    ledcWrite(pwmChannel,vel);
+}
+
+
+void loop() {
+  adelante();
+  detener();
+  atras();
+  detener();
+}
+
+
+/*
+ * Gira en sentido horario
+ *  Dir = 0 y 1
+ */
+ 
+void adelante(){
+  Serial.println("Adelante");
+  digitalWrite(motor1Pin1,dir);
+   dir = 1;
+  digitalWrite(motor1Pin2,dir);
+  delay(1000);
+}
+
+/*
+ * Gira en sentido antihorario
+ *  Dir = 1 y 0
+ */
+
+void atras(){
+  dir = 1;
+  Serial.println("Atrás");
+  digitalWrite(motor1Pin1,dir);
+  dir = 0;
+  digitalWrite(motor1Pin2,dir);
+  delay(1000);
+}
+
+void detener(){
+  dir = 0;
+  Serial.println("Detenido");
+  digitalWrite(motor1Pin1,dir);
+  digitalWrite(motor1Pin2,dir);
+  delay(500);
+}
--- README.md
+++ README.md
@@ -9,4 +9,18 @@ El link para descargar processing es:
 
 https://processing.org/download/
 
-Cuando se descarga el archivo no es necesario intalarlo ya que es portable. 
+Cuando se descarga el archivo no es necesario intalarlo ya que es portable.
+
+### Información 
+***
+
+Qué es el ADC? 
+
+- https://circuitdigest.com/tutorial/what-is-adc-analog-to-digital-converters
+
+PWM 
+
+- https://circuitdigest.com/tutorial/what-is-pwm-pulse-width-modulation
+- https://microcontrollerslab.com/esp32-pwm-arduino-ide-led-fading-example/
+- https://circuitdigest.com/microcontroller-projects/dc-motor-speed-control-using-arduino-and-potentiometer
+- https://circuitdigest.com/microcontroller-projects/esp32-pwm-tutorial-controlling-brightness-of-led 
